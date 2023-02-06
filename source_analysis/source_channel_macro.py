571c793a7baf993032087780530cfa11b81eafc3
Jacob Hsteenis jhsteenis@ucdavis.edu <jhsteenis@cms3.physics.ucsb.edu>
2023-02-06 14:54:22 -0800

    fit boundary changes & new runs (600s)

--- source_analysis/source_channel_macro.py
+++ source_analysis/source_channel_macro.py
@@ -33,18 +33,18 @@ writer2 = csv.writer(f2)
 
 for i in np.arange(len(run_number)):
     #Open this from the milliQanSourceRuns folder!
-    inputFile = r.TFile("./milliQanSourceRuns/MilliQan_Run{run}_default_v28.root".format(run=run_number[i])) #Local
-    #inputFile = r.TFile("/homes/milliqan/milliqanOffline/Run3Detector/outputRun3/v29/MilliQan_Run{run}_default_v29.root".format(run=run_number[i])) #UCSB
+    #inputFile = r.TFile("./milliQanSourceRuns/MilliQan_Run{run}_default_v28.root".format(run=run_number[i])) #Local
+    inputFile = r.TFile("/homes/milliqan/milliqanOffline/Run3Detector/outputRun3/v29/MilliQan_Run{run}_default_v29.root".format(run=run_number[i])) #UCSB
     inputTree = inputFile.Get("t")
 
     height_histo = r.TH1D("height_histo", "chan"+str(channel_cut)+";height", 200, 0, 400) #Not necessary here.
-    area_histo = r.TH1D("area_histo", "chan{chan}_run{run}; area".format(chan=channel_cut[i], run=run_number[i]), 200, 0, 20000)
+    area_histo = r.TH1D("area_histo", "chan{chan}_run{run}; area".format(chan=channel_cut[i], run=run_number[i]), 250, 0, 18750)
     oC = r.TCanvas()
 
     #Performing the cuts and filling the desired histo
     for event in inputTree:
         for iPulse in range(len(event.height)):
-            if(event.chan[iPulse] == channel_cut[i] and iPulse == 0):
+            if(event.chan[iPulse] == channel_cut[i] and event.ipulse[iPulse] == 0):
                 #print(event.sidebandMean[channel_cut[i]])
                 if(np.fabs(event.sidebandMean[channel_cut[i]]) < sbMean and np.fabs(event.sidebandRMS[channel_cut[i]]) < sbRMS):
                     area_histo.Fill(event.area[iPulse])
@@ -53,12 +53,13 @@ for i in np.arange(len(run_number)):
 
     #Performing the fits.
     f1 = r.TF1("f1", "gaus", 0, 2000)
-    f2 = r.TF1("f2", "gaus", 4000, 16000)
 
     area_histo.Fit("f1", "RL")
     r.gStyle.SetOptFit(1011)
     params1 = f1.GetParameters()
 
+    f2 = r.TF1("f2", "gaus", 5*params1[1], 20*params1[1])
+    
     area_histo.Fit("f2", "RL+")
     params2 = f2.GetParameters()
 
